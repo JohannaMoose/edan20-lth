baeb1919423be01d5dbc487bb84bf93a7bc9b26b
Johanna Petersson <min.mail@johannap.se>
2015-09-06 12:11:37 +0200

    Setup for assignment 1

---
+++ src/lppp/chapter5/Tokenizer.java
@@ -0,0 +1,32 @@
+package lppp.chapter5;
+
+import java.io.IOException;
+
+/**
+ * Created by Pierre Nugues on 25/07/15.
+ */
+public class Tokenizer {
+
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader();
+        String text = reader.readFile(args[0]);
+        Tokenizer tokenizer = new Tokenizer();
+        String[] words = tokenizer.tokenize(text);
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+        }
+    }
+
+    String[] tokenize(String text) {
+        //String [] words = text.split("[\\s\\-,;:!?.’\'«»()–...&‘’“”*—]+");
+        //String [] words = text.split("[^a-zåàâäæçéèêëîïôöœßùûüÿA-ZÅÀÂÄÆÇÉÈÊËÎÏÔÖŒÙÛÜŸ’\\-]+");
+        //String [] words = text.split("\\W+"); // Not unicode friendly
+        String[] words = text.split("\\P{L}+");
+        return words;
+    }
+
+    String tokenize2(String text) {
+        String words = text.replaceAll("\\P{L}+", " ");
+        return words;
+    }
+}
---
+++ src/se/moosetrail/skola/edan20/labbar/Main.java
@@ -0,0 +1,9 @@
+package se.moosetrail.skola.edan20.labbar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome");
+    }
+}
